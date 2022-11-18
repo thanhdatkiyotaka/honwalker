e56911b09b0184d71955280aa919f1517662380e
thanhdatkiyotaka <thanhdatkiyotakakei@gmail.com>
2022-11-18 12:37:16 +0700

    Add my project

--- src/layout/components/Footer/index.js
+++ src/layout/components/Footer/index.js
@@ -3,41 +3,55 @@ import className from 'classnames/bind';
 import style from './Footer.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faMap, faPhone } from '@fortawesome/free-solid-svg-icons';
-var cx = className.bind(style)
+import '~/assets/css/grid.css';
+var cx = className.bind(style);
 
 function Footer() {
     return (  
         <footer className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <div className={cx('conpany-info')}>
-                    <img src={require('~/assets/images/logo.png')} alt=''/>
-                    <p>Công ty cổ phần sách trực tuyến Honwalker</p>
-                </div>
-                <div className={cx('policy')}>
-                    <h1>Dịch vụ</h1>
-                    <Link className={cx('link')}>Điều khoản sử dụng</Link>
-                    <Link className={cx('link')}>Chính sách bảo mật thông tin cá nhân</Link>
-                    <Link className={cx('link')}>Chính sách bảo mật thanh toán</Link>
-                    <Link className={cx('link')}>Giới thiệu Honwalker</Link>
-                </div>
-                <div className={cx('support')}>
-                    <h1>Hỗ trợ</h1>
-                    <Link className={cx('link')}>Chính sách đổi - trả- hoàn tiền</Link>
-                    <Link className={cx('link')}>Chính sách bảo hành - bồi hoàn</Link>
-                    <Link className={cx('link')}>Chính sách vận chuyển</Link>
-                    <Link className={cx('link')}>Phương thức thanh toán và xuất hóa đơn</Link>
-                </div>
-                <div className={cx('contact')}>
-                    <h1>Liên hệ</h1>
-                    <Link className={cx('link')}>
-                        <FontAwesomeIcon icon={faMap}/>
-                        Lý Thường Kiệt, phường 14, quận 10, thành phố Hồ Chí Minh
-                    </Link>
-                    <div>
-                        <FontAwesomeIcon icon={faEnvelope}/> honwalker@gmail.com
+                <div className='grid row'>
+                    <div className='col xl-5 l-5 m-12 s-12 xs-12'>
+                        <div className={cx('conpany-info')}>
+                            <img src={require('~/assets/images/logo.png')} alt=''/>
+                            <p>Công ty cổ phần sách trực tuyến Honwalker</p>
+                        </div>
                     </div>
-                    <div>
-                        <FontAwesomeIcon icon={faPhone}/> 09xxxxxxxx
+                    <span/>
+                    <div className='col xl-7 l-7 m-12 s-12 xs-12 grid'>
+                        <div className='row'>
+                            <div className='col xl-6 l-6 m-6 xs-12 s-12'>
+                                <div className={cx('policy')}>
+                                    <h1>Dịch vụ</h1>
+                                    <Link className={cx('link')}>Điều khoản sử dụng</Link>
+                                    <Link className={cx('link')}>Chính sách bảo mật thông tin cá nhân</Link>
+                                    <Link className={cx('link')}>Chính sách bảo mật thanh toán</Link>
+                                    <Link className={cx('link')}>Giới thiệu Honwalker</Link>
+                                </div>
+                            </div>
+                            <div className='col xl-6 l-6 m-6 xs-12 s-12'>
+                                <div className={cx('support')}>
+                                    <h1>Hỗ trợ</h1>
+                                    <Link className={cx('link')}>Chính sách đổi - trả- hoàn tiền</Link>
+                                    <Link className={cx('link')}>Chính sách bảo hành - bồi hoàn</Link>
+                                    <Link className={cx('link')}>Chính sách vận chuyển</Link>
+                                    <Link className={cx('link')}>Phương thức thanh toán và xuất hóa đơn</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={cx('contact')}>
+                            <h1>Liên hệ</h1>
+                            <Link className={cx('link')}>
+                                <FontAwesomeIcon icon={faMap}/>
+                                Lý Thường Kiệt, phường 14, quận 10, thành phố Hồ Chí Minh
+                            </Link>
+                            <div>
+                                <FontAwesomeIcon icon={faEnvelope}/> honwalker@gmail.com
+                            </div>
+                            <div>
+                                <FontAwesomeIcon icon={faPhone}/> 09xxxxxxxx
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -1,6 +1,23 @@
+import '~/assets/css/grid.css'
+import className from 'classnames/bind';
+import style from './Product.module.scss';
+const cx = className.bind(style);
+
+
 function Product() {
     return (  
-        <div>Product</div>
+        <div className='grid wide'>
+            <div className='row'>
+                <div className='col xl-2 l-3 m-4 s-6 xs-12'><div className={cx('block')}>イレイナ 1</div></div>
+                <div className='col xl-2 l-3 m-4 s-6 xs-12'><div className={cx('block')}>イレイナ 2</div></div>
+                <div className='col xl-2 l-3 m-4 s-6 xs-12'><div className={cx('block')}>イレイナ 3</div></div>
+                <div className='col xl-2 l-3 m-4 s-6 xs-12'><div className={cx('block')}>イレイナ 4</div></div>
+                <div className='col xl-2 l-3 m-4 s-6 xs-12'><div className={cx('block')}>イレイナ 5</div></div>
+                <div className='col xl-2 l-3 m-4 s-6 xs-12'><div className={cx('block')}>イレイナ 6</div></div>
+                <div className='col xl-2 l-3 m-4 s-6 xs-12'><div className={cx('block')}>イレイナ 7</div></div>
+                <div className='col xl-2 l-3 m-4 s-6 xs-12'><div className={cx('block')}>イレイナ 8</div></div>
+            </div>
+        </div>
     );
 }
 
